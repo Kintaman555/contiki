b64962ee11b244486628a1634f46b6d2931b4192
Beshr <beshr@dark.cs.chalmers.se>
2015-04-01 19:57:05 +0200

    Mailing the results

--- tools/cooja/config/run-tests.sh
+++ tools/cooja/config/run-tests.sh
@@ -1,10 +1,31 @@
 #! /bin/sh
 echo "Starting tests..."
 
-java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/Users/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-no-rpl-unicast-dgm-full.csc
-java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/Users/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-no-rpl-unicast-dgm-short.csc
-java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/Users/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-sb.csc
-java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/Users/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-rb.csc
-java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/Users/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-min.csc
+mypath="/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/"
+
+java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-no-rpl-unicast-dgm-full.csc
+tar -czf $(mypath)exp.tar.gz $(mypath)*.txt  | uuencode $(mypath)exp.tar.gz exp.tar.gz | mail -s "Cooja exp" beshr@chalmers.se
+mv $(mypath)exp.tar.gz $(mypath)done
+mv $(mypath)*.txt$(mypath)done
+
+java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-no-rpl-unicast-dgm-short.csc
+tar -czf $(mypath)exp.tar.gz $(mypath)*.txt  | uuencode $(mypath)exp.tar.gz exp.tar.gz | mail -s "Cooja exp" beshr@chalmers.se
+mv $(mypath)exp.tar.gz $(mypath)done
+mv $(mypath)*.txt$(mypath)done
+
+java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-sb.csc
+tar -czf $(mypath)exp.tar.gz $(mypath)*.txt  | uuencode $(mypath)exp.tar.gz exp.tar.gz | mail -s "Cooja exp" beshr@chalmers.se
+mv $(mypath)exp.tar.gz $(mypath)done
+mv $(mypath)*.txt$(mypath)done
+
+java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-rb.csc
+tar -czf $(mypath)exp.tar.gz $(mypath)*.txt  | uuencode $(mypath)exp.tar.gz exp.tar.gz | mail -s "Cooja exp" beshr@chalmers.se
+mv $(mypath)exp.tar.gz $(mypath)done
+mv $(mypath)*.txt$(mypath)done
+
+java -d64 -mx512m -jar ../dist/cooja.jar -nogui=/home/beshr/work/contiki-private/examples/tsch-testbed/imgs/app-rpl-collect-only-min.csc
+tar -czf $(mypath)exp.tar.gz $(mypath)*.txt  | uuencode $(mypath)exp.tar.gz exp.tar.gz | mail -s "Cooja exp" beshr@chalmers.se
+mv $(mypath)exp.tar.gz $(mypath)done
+mv $(mypath)*.txt$(mypath)done
 
 echo "Done!"
