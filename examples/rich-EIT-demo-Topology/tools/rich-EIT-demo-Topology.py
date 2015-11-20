2edcdad963fd4b6a504883fa885344223877431c
Theo van Daele <theo.van.daele@nxp.com>
2015-11-20 10:24:13 +0100

    Files for EIT-ICT-RICH Topology demonstrator. CoaP resources not active yet.

---
+++ examples/rich-EIT-demo-Topology/tools/rich-EIT-demo-Topology.py
@@ -0,0 +1,113 @@
+import sys
+import time
+import socket
+import thread
+import numpy as np
+import Gnuplot
+
+# Required buffer length for each node buffer 
+SIZE_NODE_DATA = 600
+
+# Repeat time for connect watchdog and ping
+TICK = 5
+# Connect watchdog. Unit: [TICK]
+CONNECT_WATCHDOG = 30/TICK
+
+TX_PORT = 8085
+RX_PORT = 8086
+
+last_sample_value = 0
+x_range = np.arange(SIZE_NODE_DATA)
+dummy_data = np.zeros(SIZE_NODE_DATA, dtype=np.int)
+plot = Gnuplot.Data(x_range,dummy_data)
+g = Gnuplot.Gnuplot()
+
+# Attention: Skip leading zero's in IPv6 address list, otherwise address comparison with received UDP packet address goes wrong
+NODE_ALIAS = 0            # Pretty node name
+NODE_DATA = 1             # Time line with data to plot
+NODE_CONNECTED = 2        # If 0, not connected, else connect_watchdog value
+NODE_LAST_DATA = 3        # Storage for received sample
+NODE_PLOT = 4             # Plot instance
+node_data = {"bbbb::215:8d00:36:180"  : ["RICH001",     np.zeros(SIZE_NODE_DATA, dtype=np.int), 0, last_sample_value, plot],
+             "bbbb::215:8d00:36:892"  : ["RICH196",     np.zeros(SIZE_NODE_DATA, dtype=np.int), 0, last_sample_value, plot],
+             "bbbb::215:8d00:57:5b5d" : ["RICH197",     np.zeros(SIZE_NODE_DATA, dtype=np.int), 0, last_sample_value, plot],
+             "bbbb::215:8d00:36:8b1"  : ["RICH193",     np.zeros(SIZE_NODE_DATA, dtype=np.int), 0, last_sample_value, plot],
+             "bbbb::215:8d00:36:8b3" :  ["RICH198",     np.zeros(SIZE_NODE_DATA, dtype=np.int), 0, last_sample_value, plot]}
+
+# List of all nodes derived from node_data list
+node_list = node_data.keys()
+
+def initPlots():
+    for node in range(len(node_list)):
+        node_data_key = node_data[node_list[node]]
+        g.title('Rich Demonstrator: Topology Agility')
+        g.ylabel('Value')
+        g.xlabel('Time[s]')
+        g("set yrange [-150:150]")
+        g("set xrange [0:"+str(SIZE_NODE_DATA)+"]")
+        node_data_key[NODE_PLOT] = Gnuplot.Data(x_range,node_data_key[NODE_DATA], title=node_data_key[NODE_ALIAS] , with_='lines')
+    g.plot(node_data[node_list[0]][NODE_PLOT],node_data[node_list[1]][NODE_PLOT],node_data[node_list[2]][NODE_PLOT],node_data[node_list[3]][NODE_PLOT],node_data[node_list[4]][NODE_PLOT])
+    
+    
+      
+def udpReceive():
+    """RUNS ON SEPARATE THREAD """
+    while True:
+        data, addr = s_rx.recvfrom(128)
+        node_data_key = node_data[addr[0]]
+        # Indicate node is connected
+        node_data_key[NODE_CONNECTED] = CONNECT_WATCHDOG
+        print addr[0] + ' (' + node_data_key[NODE_ALIAS] + ') : ' + data
+        # Write new data at index in data buffer. Data buffer is view of plot 
+        data_lock.acquire()
+        node_data_key[NODE_LAST_DATA] = int(data)
+        data_lock.release()
+
+def plotGraphs():
+    while True:
+        data_lock.acquire()
+        for node in range(len(node_list)):
+            node_data_key = node_data[node_list[node]]
+            for k in range(1,SIZE_NODE_DATA,1):
+                node_data_key[NODE_DATA][k-1] = node_data_key[NODE_DATA][k]
+            node_data_key[NODE_DATA][SIZE_NODE_DATA-1] = node_data_key[NODE_LAST_DATA]
+            if node_data_key[NODE_CONNECTED] == 0:
+                node_data_key[NODE_PLOT] = Gnuplot.Data(x_range,node_data_key[NODE_DATA], title=node_data_key[NODE_ALIAS], with_='dots')
+            else:    
+                node_data_key[NODE_PLOT] = Gnuplot.Data(x_range,node_data_key[NODE_DATA], title=node_data_key[NODE_ALIAS], with_='lines')
+        g.plot(node_data[node_list[0]][NODE_PLOT],node_data[node_list[1]][NODE_PLOT],node_data[node_list[2]][NODE_PLOT],node_data[node_list[3]][NODE_PLOT],node_data[node_list[4]][NODE_PLOT])
+        data_lock.release()
+        time.sleep(1)    
+    
+    
+##### MAIN #####
+s_tx = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+s_rx = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+s_rx.bind(('', RX_PORT))
+initPlots()
+data_lock = thread.allocate_lock()
+thread.start_new_thread(udpReceive, ())
+thread.start_new_thread(plotGraphs, ())
+ping_node_index = 0
+ping_msg = "ping"
+while True:
+    # Every 5 secs, one node of the list in pinged
+    if (ping_node_index >=len(node_list)):
+        ping_node_index = 0;
+    try:
+        print "ping " + node_data[node_list[ping_node_index]][NODE_ALIAS]
+        s_tx.sendto(ping_msg, (node_list[ping_node_index], TX_PORT))
+    except:
+        print 'Failed to send to ' + node_list[ping_node_index]
+    ping_node_index += 1         
+    # Update connect watchdog
+    for node in range(len(node_list)):
+        node_data_key = node_data[node_list[node]]
+        if (node_data_key[NODE_CONNECTED] > 0):
+            node_data_key[NODE_CONNECTED] -= 1
+    time.sleep(TICK)    
+
+
+
+
+    
---
+++ examples/rich-EIT-demo-Topology/tools/toplogy.c
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2015, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/**
+ * \file
+ *
+ * \author Simon Duquennoy <simonduq@sics.se>
+ */
+
+#include "contiki-conf.h"
+#include "contiki-net.h"
+#include "net/ip/uip.h"
+#include "net/rpl/rpl.h"
+#include "net/rpl/rpl-private.h"
+#include "net/mac/tsch/tsch.h"
+#include "net/mac/tsch/tsch-schedule.h"
+#include "node-id.h"
+#include "orchestra.h"
+#include "toplogy.h"
+#if CONTIKI_TARGET_SKY || CONTIKI_TARGET_Z1
+#include "cc2420.h"
+#endif
+#include <string.h>
+#include <stdio.h>
+
+#define DEBUG DEBUG_PRINT
+#include "net/ip/uip-debug.h"
+
+struct toplogy_link {
+  linkaddr_t node;
+  linkaddr_t parent;
+};
+
+/* List of ID<->MAC mapping used for different deployments */
+static const struct toplogy_link topology[] = {
+    {{{0x00,0x15,0x8d,0x00,0x00,0x52,0x69,0x9a}},{{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}}}, /* Coordinator entry */
+    {{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0x92}},{{0x00,0x15,0x8d,0x00,0x00,0x52,0x69,0x9a}}}, /* RICH196 -> Coordinator */
+    {{{0x00,0x15,0x8d,0x00,0x00,0x36,0x01,0x80}},{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0x92}}}, /* RICH001 -> RICH196 */
+    {{{0x00,0x15,0x8d,0x00,0x00,0x57,0x5b,0x5d}},{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0x92}}}, /* RICH197 -> RICH196 */
+    {{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0xb3}},{{0x00,0x15,0x8d,0x00,0x00,0x52,0x69,0x9a}}}, /* RICH198 -> Coordinator */
+    {{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0xb1}},{{0x00,0x15,0x8d,0x00,0x00,0x36,0x08,0xb3}}}, /* RICH193 -> RICH198 */
+};
+
+#define NODE_COUNT (sizeof(topology)/sizeof(struct toplogy_link))
+
+/*---------------------------------------------------------------------------*/
+unsigned
+toplogy_orchestra_hash(const void *addr)
+{
+  int i;
+  for(i=0; i<NODE_COUNT; i++) {
+    if(linkaddr_cmp(addr, &topology[i].node)) {
+      return i;
+    }
+  }
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+const linkaddr_t *
+toplogy_hardcoded_parent(const linkaddr_t *addr)
+{
+  int i;
+  for(i=0; i<NODE_COUNT; i++) {
+    if(linkaddr_cmp(addr, &topology[i].node)) {
+      return &topology[i].parent;
+    }
+  }
+  return NULL;
+}
+/*---------------------------------------------------------------------------*/
+void *
+toplogy_probing_func(void *vdag)
+{
+  /* Returns the next probing target. This implementation probes the current
+   * preferred parent if we have not transmitted to it for RPL_PROBING_EXPIRATION_TIME.
+   * Otherwise, it picks at random between:
+   * (1) selecting the best parent (lowest rank) not updated for RPL_PROBING_EXPIRATION_TIME
+   * (2) selecting the least recently updated parent
+   */
+
+  rpl_dag_t *dag = (rpl_dag_t *)vdag;
+  rpl_parent_t *p;
+  rpl_parent_t *probing_target = NULL;
+  rpl_rank_t probing_target_rank = INFINITE_RANK;
+  clock_time_t probing_target_age = 0;
+  clock_time_t clock_now = clock_time();
+  rpl_parent_t *hardcoded_parent = rpl_get_parent((uip_lladdr_t *)toplogy_hardcoded_parent(&linkaddr_node_addr));
+
+  if(dag == NULL ||
+      dag->instance == NULL ||
+      dag->preferred_parent == NULL) {
+    return NULL;
+  }
+
+  /* Our preferred parent needs probing */
+  if(clock_now - dag->preferred_parent->last_tx_time > RPL_PROBING_EXPIRATION_TIME) {
+    return dag->preferred_parent;
+  }
+
+  /* Our hardcoded preferred parent needs probing */
+  if(hardcoded_parent != NULL
+      && clock_now - hardcoded_parent->last_tx_time > RPL_PROBING_EXPIRATION_TIME) {
+    return hardcoded_parent;
+  }
+
+  /* With 50% probability: probe best parent not updated for RPL_PROBING_EXPIRATION_TIME */
+  if(random_rand() % 2 == 0) {
+    p = nbr_table_head(rpl_parents);
+    while(p != NULL) {
+      if(p->dag == dag && (clock_now - p->last_tx_time > RPL_PROBING_EXPIRATION_TIME)) {
+        /* p is in our dag and needs probing */
+        rpl_rank_t p_rank = dag->instance->of->calculate_rank(p, 0);
+        if(probing_target == NULL
+            || p_rank < probing_target_rank) {
+          probing_target = p;
+          probing_target_rank = p_rank;
+        }
+      }
+      p = nbr_table_next(rpl_parents, p);
+    }
+  }
+
+  /* If we still do not have a probing target: pick the least recently updated parent */
+  if(probing_target == NULL) {
+    p = nbr_table_head(rpl_parents);
+    while(p != NULL) {
+      if(p->dag == dag) {
+        if(probing_target == NULL
+            || clock_now - p->last_tx_time > probing_target_age) {
+          probing_target = p;
+          probing_target_age = clock_now - p->last_tx_time;
+        }
+      }
+      p = nbr_table_next(rpl_parents, p);
+    }
+  }
+
+  return probing_target;
+}
